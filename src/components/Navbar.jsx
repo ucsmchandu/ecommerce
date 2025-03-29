a14a97409a0703cc3e35bfe42e705f24659c15f8
chandu <chandugit68@gmail.com>
2025-03-29 20:51:10 +0530

    navbar is partially completed without logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from 'react'
 import {motion} from 'framer-motion'
 import { Link } from 'react-router-dom'
+import close from '../assets/close.png'
+import open from '../assets/menu.png'
+import { div } from 'framer-motion/client'
 const Navbar = () => {
   const [isOpen,setIsOpen]=useState(false);
   const [hidden,setHidden]=useState(false);
@@ -30,7 +33,7 @@ const Navbar = () => {
     }}
     animate={hidden ? "hidden" : "visible"}
     transition={{duration :0.5,ease:"easeInOut"}}
-    className='flex justify-between items-center p-4 border rounded-b-lg fixed top-0 left-0 w-full z-50'>
+    className='flex justify-between items-center p-5  text-[#ffffff] bg-[#008080] fixed top-0 left-0 w-full z-50'>
       <div className='flex '>
         Logo
       </div>
@@ -40,32 +43,37 @@ const Navbar = () => {
           <motion.button
           whileHover={{scale:1.1}}
           whileTap={{scale:0.95}}
-          ><Link to="/" className=' cursor-pointer'><li className='p-1 border rounded-lg '>Home</li></Link></motion.button>
+          className='bg-[#39a2a2] rounded-lg hover:bg-[#006666] '
+          ><Link to="/" className='cursor-pointer'><li className='p-1   '>Home</li></Link></motion.button>
           <motion.button 
           whileHover={{scale:1.1}}
           whileTap={{scale:0.95}}
-          ><Link to="/shop" className=' cursor-pointer'><li className='p-1 border rounded-lg'>Shop</li></Link></motion.button>
+          className='bg-[#39a2a2] rounded-lg hover:bg-[#006666]'
+          ><Link to="/shop" className=' cursor-pointer'><li className='p-1  rounded-lg'>Shop</li></Link></motion.button>
           <motion.button 
           whileHover={{scale:1.1}}
           whileTap={{scale:0.95}}
-          ><Link to="/orders" className=' cursor-pointer'><li className='p-1 border rounded-lg'>Orders</li></Link></motion.button>
+          className='bg-[#39a2a2] rounded-lg hover:bg-[#006666]'
+          ><Link to="/orders" className=' cursor-pointer'><li className='p-1  rounded-lg'>Orders</li></Link></motion.button>
           <motion.button 
           whileHover={{scale:1.1}}
           whileTap={{scale:0.95}}
-          ><Link to="/contact" className=' cursor-pointer'><li className='p-1 border rounded-lg'>Contact us</li></Link></motion.button>
+          className='bg-[#39a2a2] rounded-lg hover:bg-[#006666]'
+          ><Link to="/contact" className=' cursor-pointer'><li className='p-1  rounded-lg'>Contact us</li></Link></motion.button>
           <motion.button 
           whileHover={{scale:1.1}}
           whileTap={{scale:0.95}}
-          ><Link to="/about" className=' cursor-pointer'><li className='p-1 border rounded-lg'>About us</li></Link></motion.button>
+          className='bg-[#39a2a2] rounded-lg hover:bg-[#006666]'
+          ><Link to="/about" className=' cursor-pointer'><li className='p-1  rounded-lg'>About us</li></Link></motion.button>
         </ul>
       </div>
       <div className='flex sm:hidden'>
        <button className='cursor-pointer border p-1 rounded-lg' onClick={()=>setIsOpen(!isOpen)}>
         {
           isOpen ? (
-            <p>Close</p>
+            <img src={close} className='h-6'/>
           ) : (
-            <p>Menu</p>
+            <img src={open} className='h-6' />
           )
         }
        </button>
@@ -73,20 +81,25 @@ const Navbar = () => {
 
     </motion.nav>
     {/* this is mobile */}
-    <div className={`flex sm:hidden w-64 border p-5 fixed top-20 z-50 left-0 h-full rounded-r-2xl transform transition-transform duration-300 ease-in-out
+    <div className={`flex sm:hidden w-64 text-white bg-[#27548A] p-5 fixed top-20 z-50 left-0 h-full rounded-r-lg transform transition-transform duration-300 ease-in-out
       ${isOpen ? "translate-x-0" : "-translate-x-full"}
       `}>
-      <ul className='w-full flex flex-col space-y-5 '>
-      <Link to="/" className='p-1'><li className='p-2 border block'>Home</li></Link>
-          <Link to="/shop" className='p-1'><li className='p-2 border block'>Shop</li></Link>
-          <Link to="/orders" className='p-1'><li className='p-2 border block'>Orders</li></Link>
-          <Link to="/contact" className='p-1'><li className='p-2 border block'>Contact us</li></Link>
-          <Link to="/about" className='p-1'><li className='p-2 border block'>About us</li></Link>
+      <ul className='w-full flex flex-col space-y-4 '>
+      <Link to="/" className='p-1 bg-[#3A6EAA] hover:bg-[#87CEEB] rounded'><li className='p-1 block text-lg'>Home</li></Link>
+          <Link to="/shop" className='p-1  bg-[#3A6EAA] hover:bg-[#87CEEB] rounded'><li className='p-1  block text-lg'>Shop</li></Link>
+          <Link to="/orders" className='p-1 bg-[#3A6EAA] hover:bg-[#87CEEB] rounded'><li className='p-1  block text-lg'>Orders</li></Link>
+          <Link to="/contact" className='p-1 bg-[#3A6EAA] hover:bg-[#87CEEB] rounded'><li className='p-1  block text-lg'>Contact us</li></Link>
+          <Link to="/about" className='p-1 bg-[#3A6EAA] hover:bg-[#87CEEB] rounded'><li className='p-1  block text-lg'>About us</li></Link>
       </ul>
     </div>
-    <div className='h-[2400px]'>
-
-    </div>
+    {
+      isOpen && (
+        <div 
+        className='fixed inset-0 bg-opacity-50 z-40'
+        onClick={()=> setIsOpen(false)}
+        />
+      )
+    }
    </>
   )
 }
