4682dc2abc7c18e7c508e980f4c8a629e01062ad
chandu <chandugit68@gmail.com>
2025-04-08 10:06:19 +0530

    functionality from the product card and product details are completed

--- src/components/Product-card.jsx
+++ src/components/Product-card.jsx
@@ -1,11 +1,25 @@
 import React, { useContext, useState } from 'react'
 import { Link } from 'react-router-dom';
+import products from '../Products-data.json';
 import WishlistContext from './Wishlist-context';
 const ProductCard = ({id,name,rating,price,quantity}) => {
     const {wishlist,setWishlist}=useContext(WishlistContext);
+    //this filters the products in wishlist with using null and undefined 
     const validWishlist=wishlist.filter((p)=>p!==null && p!=undefined);
+    //this is checks the product in wishlist if not present then add the product into wishlist
     const findProductInWishlist=validWishlist.find((p)=>p.id===id);
-    
+    //this adds the product into wishlist
+    const addToWishlist=(item)=>{
+     if(!item || !item.id || !item.name){
+      console.error("Product not found in product-card.jsx",item);
+      return;
+     }
+     if(!findProductInWishlist){
+      setWishlist([...validWishlist,{...item,quantity:1}]);
+     }
+    };
+    const filterProducts=products.filter((p)=>p!=null && p!=undefined);
+    const findProductInProducts=filterProducts.find((p)=>p.id===id);
   return (
         <div className='w-72  flex flex-col items-center p-2 bg-gray-100 rounded-lg shadow-md '>
         <div className='p-2'>
@@ -20,9 +34,10 @@ const ProductCard = ({id,name,rating,price,quantity}) => {
             <p className='text-amber-500'>{rating}★★★★☆</p>
             <p className='text-lg'>{price}<span className="relative left-1">₹</span></p>
             <button className='font-medium cursor-pointer'
+            onClick={()=>addToWishlist(findProductInProducts)}
             >
                 {
-                  !findProductInWishlist ?  <span className='text-blue-500 hover:text-blue-600'>Add to wishlist</span>:<span>Visit wishlist</span>
+                  !findProductInWishlist ?  <span className='text-blue-500 hover:text-blue-600'>Add to wishlist</span>:<span><Link to="/wishlist">Visit wishlist</Link></span>
                 }
             </button>
         </div>
--- src/pages/Product-details.jsx
+++ src/pages/Product-details.jsx
@@ -1,27 +1,24 @@
 import React, { useContext, useState } from "react";
+import { Link } from "react-router-dom";
 import CartContext from '../components/Cart-context'
+import WishlistContext from "../components/Wishlist-context";
 import ProductsListings from "../components/Products-listings";
 import { useParams } from "react-router-dom";
 import products from '../Products-data.json'
 const ProductDetails = () => {
-  
   // initializing the cart array object by using the contextApi
   const {cart,setCart}=useContext(CartContext);
     //here is the function to add the items into cart
   const addToCart = (item) => {
-  
     // Validate the incoming item
     if (!item || !item.id || !item.price) {
       console.error("Invalid product data:", item);
       return;
     }
-  
     // Filter out null or undefined items from the cart
     const validCart = cart.filter((p) => p !== null && p !== undefined);
-  
     // Check if the product already exists in the cart
     const existingProduct = validCart.find((p) => p.id === item.id);
-  
     if (existingProduct) {
       setCart(
         validCart.map((cartItem) =>
@@ -34,6 +31,19 @@ const ProductDetails = () => {
       setCart([...validCart, { ...item, quantity: 1 }]);
     }
   };
+
+  const {wishlist,setWishlist}=useContext(WishlistContext);
+  const filterWishlist=wishlist.filter((p)=>p!=null && p!=undefined);
+  const addProductToWishlist=(item)=>{
+    if(!item || !item.id || !item.name){
+      console.error("Product not found in product-details :",item);
+      return;
+    }
+  const findProductInWishlist=filterWishlist.find((p)=>p.id===item.id);
+    if(!findProductInWishlist){
+      setWishlist([...filterWishlist,{...item,quantity:1}]);
+    }
+  }
   // taking the url value using params
   const {name}=useParams();
   //checking
@@ -100,8 +110,14 @@ if (!product) {
           </div>
           <div>
             {/* add to wishlist */}
-            <button className="w-full px-4 py-2 bg-gray-600 text-white rounded hover:bg-gray-700 cursor-pointer">
-              Add to wishlist
+            <button
+            onClick={()=>addProductToWishlist(product)}
+            className="w-full px-4 py-2 bg-gray-600 text-white rounded hover:bg-gray-700 cursor-pointer">
+             {
+              !filterWishlist.find((p)=>p.id===product.id) ?
+              <span>Add to wishlist</span> :
+              <span><Link to="/wishlist">Visit wishlist</Link></span>
+              }
             </button>
           </div>
         </div>
